1b3a324789ef17da0b88b1062556dcc76317f6d3
Alex Zellner <37265689+zellneralex@users.noreply.github.com>
2023-11-18 10:56:24 +0100

    change result path

--- K-ShakeTune/scripts/is_workflow.py
+++ K-ShakeTune/scripts/is_workflow.py
@@ -35,7 +35,7 @@ import tarfile
 from datetime import datetime
 
 #################################################################################################################
-RESULTS_FOLDER = os.path.expanduser('~/printer_data/config/K-ShakeTune_results')
+RESULTS_FOLDER = os.path.expanduser('~/printer_data/config/ShakeTuneResults')
 KLIPPER_FOLDER = os.path.expanduser('~/klipper')
 STORE_RESULTS = 3
 #################################################################################################################
